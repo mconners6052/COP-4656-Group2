da8044e0e6ec05ea14903c7147d9dfa8bf1fa97a
Joshua Swonger <jswonger1121@gmail.com>
2018-11-28 11:35:43 -0500

    Updated Home Screen

--- SmartMuscle/app/src/main/java/com/fitness/sm/smartmuscle/HomeFragment.java
+++ SmartMuscle/app/src/main/java/com/fitness/sm/smartmuscle/HomeFragment.java
@@ -3,9 +3,15 @@ package com.fitness.sm.smartmuscle;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 
 /**
@@ -13,8 +19,24 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+    Calendar calendar = Calendar.getInstance();
+    private int mCurrentIndex = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+    private ImageView mWorkoutButton;
+    private TextView mQuoteTextView;
+    private View homeView;
+    private WorkoutFragment wof;
+    private Quote[] mQuoteBank = new Quote[] {
+            new Quote(R.string.quote00),
+            new Quote(R.string.quote01),
+            new Quote(R.string.quote02),
+            new Quote(R.string.quote03),
+            new Quote(R.string.quote04),
+            new Quote(R.string.quote05),
+            new Quote(R.string.quote06),
+    };
 
     public HomeFragment() {
+        wof = new WorkoutFragment();
         // Required empty public constructor
     }
 
@@ -22,8 +44,47 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        homeView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //Changes Quote of the day
+        mQuoteTextView = homeView.findViewById(R.id.quote_text_view);
+        updateQuote();
+
+        //Workout Button Clickable
+        mWorkoutButton = homeView.findViewById(R.id.workout_button);
+        mWorkoutButton.setImageResource(R.drawable.ic_back_day);
+        mWorkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Toast.makeText(homeView.getContext(),"WORKOUT",Toast.LENGTH_LONG).show();
+                FragmentManager fm = getFragmentManager();
+                fm.beginTransaction().replace(R.id.main_frame, wof).commit();
+            }
+        });
+
+        return homeView;
+    }
+
+    private void updateQuote()
+    {
+        int quote = mQuoteBank[mCurrentIndex].getmTextResID();
+        mQuoteTextView.setText(quote);
+    }
+
+    private class Quote
+    {
+        int mTextResID;
+
+        public Quote(int TextResID)
+        {
+            mTextResID = TextResID;
+        }
+
+        public int getmTextResID() {
+            return mTextResID;
+        }
     }
 
 }
